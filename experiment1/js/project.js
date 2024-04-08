c584c9f27af47a0a2396c06e8e85c585834f75e4
EldritchSalamancer <147461070+CCroyPerrett@users.noreply.github.com>
2024-04-07 20:22:34 -0700

    Experiment 1, Getting Started

--- experiment1/js/project.js
+++ experiment1/js/project.js
@@ -1,34 +1,64 @@
-// project.js - purpose and description here
-// Author: Your Name
-// Date:
+// project.js - A murder mystery Story Generator
+// Author: Chase Croy-Perrett
+// Date:4/7/2024
 
-// NOTE: This is how we might start a basic JavaaScript OOP project
-
-// Constants - User-servicable parts
-// In a longer project I like to put these in a separate file
-
-// define a class
-class MyProjectClass {
-  // constructor function
-  constructor(param1, param2) {
-    // set properties using 'this' keyword
-    this.property1 = param1;
-    this.property2 = param2;
-  }
+const fillers = {
+  location: ["the town's spooky mansion", "the local park", "the highest skyskraper in the city", "your own home", "the school", "a cabin in the woods", "a train", "the victim's birthday party", "a cosplay convention", "a secret government complex"],
+  protagonist: ["detective", "comissioner", "lieutenant", "officer", "investigator", "curious child"],
+  victim: ["Sir Nickelbottom the 2nd", "The mayor, Can Townings", "Your best friend", "A visiting billionaire, Melon Tusk", "The cafeteria manager, Yume Nikki", "The chief of police, Yuki Tsurugi ", "The local college's dean, Sara Stevens", "The most disliked teacher John Lemmings", "Knight Peko Pekoyama"],
+  body_state: ["decapitated", "scorched", "strangled", "drowned", "electrocuted", "hung", "on a crucifix", "without a single wound", "with blood coming up the mouth", "buried", "exploded", "crushed", "shot", "pierced through the heart", "bludgeoned", "bitten", "claiming to still be alive, which I personally do not believe"],
+  item: ["a bloody axe", "a gun", 'a pipebomb', "a love confession letter", "your wallet", "a heavy pipe", "100 thousand dollars", "a  sword", "a red glove", "a sack of toys", "a pool noodle", "the death note", "a broken computer", "rope", "librarian card", "an adorable puppy", "my lunch", "a fake police bade", "a pair of bloody sissors", "a metal bat", "a cease and disist"],
+  suspect: ["Asahina", "Togami", "Hagakure", "Naegi", "Kigiri", "Hinata", "Kuzuryu", "Souda", "Owari", "Saionji", "Saihara", "Harumaki", "Yumeno", "Keebo", "Teruya", "Kinjo", "Mekaru", "Gill", "Nichole", "Tabitha", "Eliza", "Moose", "Walter", "Jaiden", "Zoey", "Edgeworth", "Mia", "Ibuki", "Joe", "Alex", "Steve", "William", "Mike"],
+  profession: ["librarian", "fireman", "serial killer", "police officer", "bulter", "priest", "lifeguard", "baseball star", "chef", "maid", "astronaught", "clown", "gambler", "fashion icon", "lucky student", "piano player", "nurse", "animal tamer", "gamer", "anthropologist", "entomologist", "detective", "my mom", "teacher", "merchant", "lawyer"],
+  trait: ["shy", "confident", "kind", "manical", "studios", "psycho", "egotistical", "eccentric", "lazy", "mysterious", "cruel", "adventerous", "creative", "optimistic", "depressed", "confident", "flirty", "intelligant", "obnoxious", "sleepy", "scared", "creepy"],
+  emotion: ["dismayed", "distressed", "confused", "overjoyed", "saddened", "frothing"],
+  reward: ["the key to the city", "my wallet", "my hand in marrage", "an ice cream", "a really cool hat", "19 dollar fortnite card", "a new car", "the big cheese", "a real life pikachu", "a nintendo switch", "scp-999"]
   
-  // define a method
-  myMethod() {
-    // code to run when method is called
+  
+  
+};
+
+/*const template = `$adventurer, heed my $message!
+
+I have just come from $pre$post where the $people folk are in desperate need. Their town has been overrun by $baddies. You must venture forth at once, taking my $item, and help them.
+
+It is told that the one who can rescue them will be awarded with $num $looty $loots. Surely this must tempt one such as yourself!
+`;*/
+
+const template = `Thank goodness you are here $protagonist! I am $emotion to announce... a body has been discovered!\t
+
+There has been a murder! $victim has been killed at $location. Their body was $body_state, and we found $item at the location.\n\n
+
+Luckily, we have captured 3 suspects at the scene of the crime! $suspect, a $trait $profession. $suspect, a $trait $profession. $suspect, a $trait $profession.\n\n
+
+If you solve the murder, we will give you $reward. Please! You are our only hope!
+`;
+
+
+// STUDENTS: You don't need to edit code below this line.
+
+const slotPattern = /\$(\w+)/;
+
+function replacer(match, name) {
+  let options = fillers[name];
+  if (options) {
+    return options[Math.floor(Math.random() * options.length)];
+  } else {
+    return `<UNKNOWN:${name}>`;
   }
 }
 
-function main() {
-  // create an instance of the class
-  let myInstance = new MyProjectClass("value1", "value2");
+function generate() {
+  let story = template;
+  while (story.match(slotPattern)) {
+    story = story.replace(slotPattern, replacer);
+  }
 
-  // call a method on the instance
-  myInstance.myMethod();
+  /* global box */
+  $("#box").text(story);
 }
 
-// let's get this party started - uncomment me
-//main();
+/* global clicker */
+$("#clicker").click(generate);
+
+generate();
